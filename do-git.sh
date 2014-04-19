f948a3c924b5d793c4cb79276a37bdea7ffc22d5
SomniusX <lefteros@me.com>
2014-04-20 01:38:23 +0300

    added more to do-it.sh and changed stuff

--- do-git.sh
+++ do-git.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo " "
 zip -r -q -9 ../MIUI-XML-5.0-Greek_Backup/backup-$(date +%Y%m%d_%H%M).zip *
+#ls -t ../MIUI-XML-5.0-Greek_Backup/*.zip | head -1
+du -sh ../MIUI-XML-5.0-Greek_Backup/*.zip|tail -1
 sleep 1
 git commit -a
 git pull
