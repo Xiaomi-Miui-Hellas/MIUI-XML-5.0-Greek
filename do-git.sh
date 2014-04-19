a3c70871095ecc4d6ffd68280290e92628d6bd0c
SomniusX <lefteros@me.com>
2014-04-20 01:26:43 +0300

    added do-git.sh with backup command.. needs folder

--- do-git.sh
+++ do-git.sh
@@ -1,1 +1,10 @@
-git commit -a && git pull && git push
+#!/bin/bash
+echo " "
+zip -r -q -0 ../MIUI-XML-5.0-Greek_Backup/backup-$(date +%Y%m%d_%H%m).zip *
+sleep 1
+git commit -a
+git pull
+git push
+echo " "
+echo "All done.."
+echo " "
