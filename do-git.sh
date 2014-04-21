9c6bb9c95d3fb9a8cfc550db11d6c375bdb164bc
SomniusX <lefteros@me.com>
2014-04-21 14:20:39 +0300

    changed do-git to check if backup directory exists

--- do-git.sh
+++ do-git.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
-echo "Starting process.."
-zip -r -q -9 ../MIUI-XML-5.0-Greek_Backup/backup-$(date +%Y%m%d_%H%M).zip *
-#ls -t ../MIUI-XML-5.0-Greek_Backup/*.zip | head -1
-du -sh ../MIUI-XML-5.0-Greek_Backup/*.zip|tail -1
-sleep 1
-git commit -a
-git pull
-git push
-#echo " "
-echo "All done.."
-#echo " "
+if [ -d "../MIUI-XML-5.0-Greek_Backup" ]
+then
+    echo "Backup Directory exists."
+	echo "Starting process.."
+	zip -r -q -9 ../MIUI-XML-5.0-Greek_Backup/backup-$(date +%Y%m%d_%H%M).zip *
+	#ls -t ../MIUI-XML-5.0-Greek_Backup/*.zip | head -1
+	du -sh ../MIUI-XML-5.0-Greek_Backup/*.zip|tail -1
+	sleep 1
+	git commit -a
+	git pull
+	git push
+	#echo " "
+	echo "All done.."
+	#echo " "
+else
+    echo "Backup Directory doesn't exist, creating.."
+	mkdir ../MIUI-XML-5.0-Greek_Backup
+	echo "Starting process.."
+	zip -r -q -9 ../MIUI-XML-5.0-Greek_Backup/backup-$(date +%Y%m%d_%H%M).zip *
+	#ls -t ../MIUI-XML-5.0-Greek_Backup/*.zip | head -1
+	du -sh ../MIUI-XML-5.0-Greek_Backup/*.zip|tail -1
+	sleep 1
+	git commit -a
+	git pull
+	git push
+	#echo " "
+	echo "All done.."
+	#echo " "
+fi
