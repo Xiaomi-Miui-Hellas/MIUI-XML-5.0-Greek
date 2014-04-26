9f64c12641a98de314d66a603434eee190f687c7
Somnius <lefteros@me.com>
2014-04-26 16:18:56 +0300

    playing with do-git.sh from work, will update it

--- do-git.sh
+++ do-git.sh
@@ -3,9 +3,13 @@ if [ -d "../MIUI-XML-5.0-Greek_Backup" ]
 then
     echo "Backup Directory exists."
 	echo "Starting process.."
+#	mkdir -p /tmp
 	zip -r -q -9 ../MIUI-XML-5.0-Greek_Backup/backup-$(date +%Y%m%d_%H%M).zip *
 	#ls -t ../MIUI-XML-5.0-Greek_Backup/*.zip | head -1
 	du -sh ../MIUI-XML-5.0-Greek_Backup/*.zip|tail -1
+	echo "Backup folder size is.."
+	du -sh ../MIUI_XML-5.0-Greek_Backup/
+	echo " "
 	sleep 1
 	git commit -a
 	git pull
@@ -16,10 +20,14 @@ then
 else
     echo "Backup Directory doesn't exist, creating.."
 	mkdir ../MIUI-XML-5.0-Greek_Backup
+#	mkdir -p /tmp
 	echo "Starting process.."
 	zip -r -q -9 ../MIUI-XML-5.0-Greek_Backup/backup-$(date +%Y%m%d_%H%M).zip *
 	#ls -t ../MIUI-XML-5.0-Greek_Backup/*.zip | head -1
 	du -sh ../MIUI-XML-5.0-Greek_Backup/*.zip|tail -1
+	echo "Backup folder size is.."
+	du -sh ../MIUI-XML-5.0-Greek_Backup/
+	echo " "
 	sleep 1
 	git commit -a
 	git pull
