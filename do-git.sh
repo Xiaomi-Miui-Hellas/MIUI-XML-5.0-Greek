4ad6ea3c147f6a33911b4af7b10168022d235ad9
Somnius <lefteros@me.com>
2014-04-27 12:49:22 +0300

    another small change in do-git.sh script

--- do-git.sh
+++ do-git.sh
@@ -7,7 +7,7 @@ echo "Do GIT script v.$VER"
 echo "by @SomniusX (Somnius on GitHub)"
 echo " "
 echo "The program checks for backup directory, if it doesn't exist it creates it."
-echo "Then it zip's with -r -q -9 flags (recursive/quite/max compression) with date."
+echo "Then it zip's with -r -q -9 flags (recursive/quiet/max compression) with date."
 echo "Then it displays backup folder size and starts commit all, pull and then push."
 echo " "
 echo "-------------------------------------------------------"
@@ -19,10 +19,10 @@ then
 # the zipping using date
 	zip -r -q -9 $BACKPATH/backup-$(date +%Y%m%d_%H%M).zip *
 # the du to check the file size
-	du -sh $BACKPATH/*.zip|tail -1
+	du -sh $BACKPATH/*.zip|tail -1|sed -e "s/..\/MIUI-XML-5.0-Greek_/is the back'ed up file\: /"
 	echo "Backup folder size is.."
 # the du with sed to display the backup folder size
-	du -sh $BACKPATH | sed -e "s/..\/MIUI-XML-5.0-Greek_/holds the folder\: /"
+	du -sh $BACKPATH|sed -e "s/..\/MIUI-XML-5.0-Greek_/holds the folder\: /"
 	echo " "
 # wait for 1sec
 	sleep 1
@@ -43,10 +43,10 @@ else
 # the zipping using date
 	zip -r -q -9 $BACKPATH/backup-$(date +%Y%m%d_%H%M).zip *
 # the du to check the file size
-	du -sh $BACKPATH/*.zip|tail -1
+	du -sh $BACKPATH/*.zip|tail -1|sed -e "s/..\/MIUI-XML-5.0-Greek_/is the back'ed up file\: /"
 	echo "Backup folder size is.."
 # the du with sed to display the backup folder size
-	du -sh $BACKPATH | sed -e "s/..\/MIUI-XML-5.0-Greek_/holds the folder\: /"
+	du -sh $BACKPATH|sed -e "s/..\/MIUI-XML-5.0-Greek_/holds the folder\: /"
 	echo " "
 # wait for 1sec
 	sleep 1
