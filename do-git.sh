a848b54987009c5b1501f1f05a511ea8baac716a
Somnius <lefteros@me.com>
2014-04-27 12:45:02 +0300

    changed do-git.sh, added features

--- do-git.sh
+++ do-git.sh
@@ -1,38 +1,62 @@
 #!/bin/bash
-if [ -d "../MIUI-XML-5.0-Greek_Backup" ]
+clear
+# variables
+BACKPATH="../MIUI-XML-5.0-Greek_Backup"
+VER="0.6"
+echo "Do GIT script v.$VER"
+echo "by @SomniusX (Somnius on GitHub)"
+echo " "
+echo "The program checks for backup directory, if it doesn't exist it creates it."
+echo "Then it zip's with -r -q -9 flags (recursive/quite/max compression) with date."
+echo "Then it displays backup folder size and starts commit all, pull and then push."
+echo " "
+echo "-------------------------------------------------------"
+echo " " 
+if [ -d $BACKPATH ]
 then
     echo "Backup Directory exists."
 	echo "Starting process.."
-#	mkdir -p /tmp
-	zip -r -q -9 ../MIUI-XML-5.0-Greek_Backup/backup-$(date +%Y%m%d_%H%M).zip *
-	#ls -t ../MIUI-XML-5.0-Greek_Backup/*.zip | head -1
-	du -sh ../MIUI-XML-5.0-Greek_Backup/*.zip|tail -1
+# the zipping using date
+	zip -r -q -9 $BACKPATH/backup-$(date +%Y%m%d_%H%M).zip *
+# the du to check the file size
+	du -sh $BACKPATH/*.zip|tail -1
 	echo "Backup folder size is.."
-	du -sh ../*_Backup
+# the du with sed to display the backup folder size
+	du -sh $BACKPATH | sed -e "s/..\/MIUI-XML-5.0-Greek_/holds the folder\: /"
 	echo " "
+# wait for 1sec
 	sleep 1
+# the commit all
 	git commit -a
+# the pull
 	git pull
+# the push
 	git push
-	#echo " "
+#echo " "
 	echo "All done.."
-	#echo " "
+#echo " "
 else
     echo "Backup Directory doesn't exist, creating.."
-	mkdir ../MIUI-XML-5.0-Greek_Backup
-#	mkdir -p /tmp
+# creating the backup directory for 1st time use
+	mkdir $BACKPATH
 	echo "Starting process.."
-	zip -r -q -9 ../MIUI-XML-5.0-Greek_Backup/backup-$(date +%Y%m%d_%H%M).zip *
-	#ls -t ../MIUI-XML-5.0-Greek_Backup/*.zip | head -1
-	du -sh ../MIUI-XML-5.0-Greek_Backup/*.zip|tail -1
+# the zipping using date
+	zip -r -q -9 $BACKPATH/backup-$(date +%Y%m%d_%H%M).zip *
+# the du to check the file size
+	du -sh $BACKPATH/*.zip|tail -1
 	echo "Backup folder size is.."
-	du -sh ../*_Backup
+# the du with sed to display the backup folder size
+	du -sh $BACKPATH | sed -e "s/..\/MIUI-XML-5.0-Greek_/holds the folder\: /"
 	echo " "
+# wait for 1sec
 	sleep 1
+# the commit all
 	git commit -a
+# the pull
 	git pull
+# the push
 	git push
-	#echo " "
+#echo " "
 	echo "All done.."
-	#echo " "
+#echo " "
 fi
